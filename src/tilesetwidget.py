bc8e40573ab00b29a38f15db48367ee8a5e3ad5a
mnankman <mark.nankman@gmail.com>
2020-07-28 17:43:51 +0200

    improved highlighting of sets

--- src/tilesetwidget.py
+++ src/tilesetwidget.py
@@ -6,6 +6,8 @@ import util
 class TileSetWidget(DragablePanel):  
     normalPenColor = 'Black'
     highlightPenColor = 'White'
+    modifiedPenColor = '#008800'
+    modifiedHighlightPenColor = '#00FF00'
     def __init__(self, parent, set):
 #        super().__init__(parent=parent, style=wx.TRANSPARENT_WINDOW)
         super().__init__(parent=parent)
@@ -25,9 +27,15 @@ class TileSetWidget(DragablePanel):
 
     def draw(self,dc):
         if self.highlight:
-            dc.SetPen(wx.Pen(TileSetWidget.highlightPenColor, 1, wx.PENSTYLE_DOT))
+            if self.set.isModified():
+                dc.SetPen(wx.Pen(TileSetWidget.modifiedHighlightPenColor, 1, wx.PENSTYLE_DOT))
+            else:
+                dc.SetPen(wx.Pen(TileSetWidget.highlightPenColor, 1, wx.PENSTYLE_DOT))
         else:
-            dc.SetPen(wx.Pen(TileSetWidget.normalPenColor, 1, wx.PENSTYLE_DOT))
+            if self.set.isModified():
+                dc.SetPen(wx.Pen(TileSetWidget.modifiedPenColor, 1, wx.PENSTYLE_DOT))
+            else:
+                dc.SetPen(wx.Pen(TileSetWidget.normalPenColor, 1, wx.PENSTYLE_DOT))
         dc.SetBrush(wx.TRANSPARENT_BRUSH)
         w,h = self.GetClientSize()
         dc.DrawRoundedRectangle(0,0,w,h,6)
@@ -44,6 +52,7 @@ class TileSetWidget(DragablePanel):
         if tile.move(self.set):
             w,h = self.GetClientSize()
             self.SetSize(len(self.set.getTiles())*36+6, h)
+            self.highlight = False
             self.Refresh()
 
 
